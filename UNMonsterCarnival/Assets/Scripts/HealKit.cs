22bd1005c9e73f225735dfeb48126af617f1e576
SAMSUNG\USER <ljm245@naver.com>
2017-11-25 23:55:00 +0900

    20171125 23:53 heal,jungle

--- UNMonsterCarnival/Assets/Scripts/HealKit.cs
+++ UNMonsterCarnival/Assets/Scripts/HealKit.cs
@@ -11,31 +11,20 @@ public class HealKit : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        take_HealKit = false;
-        timeSpan = 0;
-        checkTime = 10;
+   
 	}
 	
     void OnTriggerEnter2D(Collider2D other)
     {
-        if(other.gameObject.tag=="Player"&&take_HealKit==false)
+        if(other.gameObject.tag=="Player")
         {
             GameObject.Find("Player").GetComponent<Player>().Heal(healHP);
-            take_HealKit = true;
-            //안보이게하기
+            //take_HealKit = true;
+            gameObject.SetActive(false);
         }
     }
 	// Update is called once per frame
 	void Update () {
-        if (take_HealKit == true)
-        {
-            timeSpan += Time.deltaTime;
-        }
-        if (timeSpan > checkTime)
-        {
-            timeSpan = 0;
-            take_HealKit = false;
-            //다시 보이게
-        }
+     
 	}
 }
---
+++ UNMonsterCarnival/Assets/Scripts/Tower.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum State
+{
+    noHost,user1,user2
+}
+
+public class Tower : Battleable{
+    public int towerHost;
+    // Use this for initialization
+    void Start()
+    {
+        //towerHost = noHost;
+        HP = 300;
+        Power = 20;
+        DelayAttack = 2.0d;
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
